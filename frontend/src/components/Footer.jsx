1231c38bf8babcf714b6d10bf22d25074ab7992c
Divya Kumari <divyakumari0093@gmail.com>
2026-05-21 10:55:10 +0530

    changes ahs did

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -13,7 +13,7 @@ const Footer = () => {
 
         {/**------------Center Section ---------- */}
         <div>
-          <p className='text-xl font-medium mb-5'>COMPANY IS</p>
+          <p className='text-xl font-medium mb-5'>COMPANY</p>
           <ul className='flex flex-col gap-2 text-gray-600'>
             <li>Home</li>
             <li>About us</li>
